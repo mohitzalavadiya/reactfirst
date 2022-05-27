bc3840807da0d6cfcd75a5a550186e6d3a06a58b
mohitzalavadiya <mzpatel14@gmail.com>
2022-05-27 15:19:55 +0530

    message

---
+++ src/component/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                my name mohit
+            </div>
+        );
+    }
+}
+
+export default Demo;
