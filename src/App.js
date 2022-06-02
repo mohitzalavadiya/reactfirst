63ecdf09ed889f3f89ab14b337dc110ac1c37dac
mohitzalavadiya <mzpatel14@gmail.com>
2022-06-02 11:43:59 +0530

    chnage

--- src/App.js
+++ src/App.js
@@ -3,78 +3,113 @@ import './App.css';
 import Demo from './component/Demo';
     
 
-const Data= 
-[
-   {
-     id: 101,
-     name: 'Abacavir',
-     quantity: 25,
-     price: 150,
-     expiry: 2022,
-     status: true
-   },
-   {
-     id: 102,
-     name: 'Eltrombopag',
-     quantity: 90,
-     price: 550,
-     expiry: 2021,
-     status: true
-   },
-   {
-     id: 103,
-     name: 'Meloxicam',
-     quantity: 85,
-     price: 450,
-     expiry: 2025,
-     status: false
-   },
-   {
-     id: 104,
-     name: 'Allopurinol',
-     quantity: 50,
-     price: 600,
-     expiry: 2023,
-     status: true
-   },
-   {
-     id: 105,
-     name: 'Phenytoin',
-     quantity: 63,
-     price: 250,
-     expiry: 2021,
-     status: false
-   }
- ];
- 
+const person_1 = {
+  name: "Amit",
+  age: 25
+}
+
+
+const person_2 = [
+  {
+      name: "Amit",
+      age: 25,
+  },
+  {
+      name: "Piyush",
+      age: 40,
+  },
+
+
+];
+
+
+const person_3 = {
+  name: "Amit",
+  age: 25,
+  course: [
+      "C",
+      "HTML"
+  ]
+}
+
+
+const person_4 = [
+  {
+      name: "Amit",
+      age: 25,
+      course: [
+          "C",
+          "HTML"
+      ]
+  },
+  {
+      name: "Ajay",
+      age: 40,
+      course: [
+          "Java",
+          "JavaScript"
+      ]
+  }
+]
+
+
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: {
+      car1: "Ford",
+      car2: "BMW",
+      car3: "Fiat"
+  }
+}
+
+const data = {
+  personal_info: {
+      name: 'amit',
+      age: 25,
+      city: 'surat'
+  },
+  courses: ["C", "JavaScript", "React"],
+  branches: {
+      rwl: {
+          admission: 50,
+          vacant_seat: 10
+      },
+      rw2: {
+          admission: 30,
+          vacant_seat: 20
+      },
+      rw3: {
+          admission: 25,
+          vacant_seat: 25
+      },
+      rw4: {
+          admission: 40,
+          vacant_seat: 10
+      }
+  }
+}
+
+
+person_2.map((p.i) => {
+
+})
 
 function App() {
   return (
-    <table border="1" cellPadding="2" cellSpacing="0"> 
-      <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Expiry</th>
-        <th>Status</th>
-      </tr>
-      {
-        Data.map((v, i) => {
-          const {id, name, quantity, price, expiry, status} = v;
-          return(
-          <tr>
-            <td>{id}</td>
-            <td>{name}</td>
-            <td>{quantity}</td>
-            <td>{price}</td>
-            <td>{expiry}</td>
-            <td>{status.toString()}</td>
-          </tr>
-          )
-        })
-      }
-    </table>
+    <div>
+      <ul>
+        <li>{person_1.name}</li>
+        <li>{person_1.age}</li>
+      </ul>
+
+    <ul>
+    
+      
+    </ul>
+     
+    </div>
+
 
   )
 }
